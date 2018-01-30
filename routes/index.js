db27c9673720c81b11b006fab584eb211c17dfd6
JohnnyBui <phuc.m.bui@gmail.com>
2018-01-30 14:06:11 +0700

    new massage api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
+const TELEGRAM_BOT_API_KEY = process.env.TELEGRAM_BOT_API_KEY;
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Okos - Johnny Assistant' });
+});
+
+router.post('/new-message', function(req, res, next) {
+  const message = req.body.message;
+
+  axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_API_KEY}/sendMessage`, {
+    chat_id: message.chat.id,
+    text: `Hello world! I heard ${message.text}`
+  }).then(response => {
+      res.send('ok');
+  }).catch(err => {
+    res.send('Error :' + err);
+  });
 });
 
 module.exports = router;
